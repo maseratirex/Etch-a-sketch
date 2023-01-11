f89101df3ad7f84c99ffdd61c839410722b7abb5
maseratirex <r.jamesthomaswalker@gmail.com>
2023-01-11 17:51:13 -0500

    Create grid and add hover effect

---
+++ script.js
@@ -0,0 +1,23 @@
+const grid = document.querySelector(".grid")
+window.addEventListener("load", createGrid(16)) //on load, create a 16x16 blank grid
+
+function createGrid(size){
+    const cellSize = 640/size;
+    for(let row = 0; row < size; row++){
+        const row = document.createElement("div")
+        row.style.display = "flex"
+        for(let column = 0; column < size; column++){
+            const cell = document.createElement("div")
+            cell.classList.add("cell")
+            cell.style.width = `${cellSize}px`
+            cell.style.height = `${cellSize}px`
+            row.appendChild(cell);
+        }
+        grid.appendChild(row)
+    }
+}
+
+const cells = grid.querySelectorAll(".cell")
+cells.forEach((cell) => {
+    cell.addEventListener("mouseover", () => cell.classList.add("hovered"))
+})
